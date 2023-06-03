39a22fe412c4ab23d40effecbc811782cc6d47ea
Sanchit Batra <sanchitbatra@Sanchits-MacBook-Air.local>
2023-06-03 16:34:00 +0530

    matching theme with website

---
+++ android/app/src/main/kotlin/com/example/kidys_distributor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kidys_distributor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          // ignore: prefer_const_literals_to_create_immutables
+          gradient: LinearGradient(
+              colors: [Color(0xffE6E3D3), Color(0xffE6E3D3)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight)),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/logo.png",
+              height: 250,
+              width: double.infinity,
+              fit: BoxFit.contain,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CupertinoButton(
+              onPressed: () {},
+              color: Color(0xffdd0e1c),
+              child: Text(
+                "LOGIN",
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CupertinoButton(
+              onPressed: () {},
+              color: Color(0xffdd0e1c),
+              child: Text(
+                "SIGN UP",
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

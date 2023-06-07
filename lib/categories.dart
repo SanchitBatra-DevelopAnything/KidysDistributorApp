06033885de7b68643c84801c9aceddbd72b9158a
Sanchit Batra <sanchitbatra@Sanchits-MacBook-Air.local>
2023-06-07 23:21:19 +0530

    save b4 fucking up

--- lib/categories.dart
+++ lib/categories.dart
@@ -10,131 +10,117 @@ class Categories extends StatefulWidget {
 class _CategoriesState extends State<Categories> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFFDD0E1C),
-      body: ListView(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(top: 15.0, left: 10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.arrow_back_ios),
-                  color: Colors.white,
-                  onPressed: () {},
-                ),
-                Container(
-                    width: 125.0,
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xFFDD0E1C),
+        body: ListView(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 15.0, left: 10.0),
+              child: Column(
+                children: [
+                  Container(
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: <Widget>[
                         IconButton(
-                          icon: Icon(Icons.filter_list),
+                          icon: Icon(Icons.arrow_back_ios),
                           color: Colors.white,
                           onPressed: () {},
                         ),
-                        IconButton(
-                          icon: Icon(Icons.menu),
-                          color: Colors.white,
-                          onPressed: () {},
-                        )
-                      ],
-                    ))
-              ],
-            ),
-          ),
-          SizedBox(height: 25.0),
-          Padding(
-            padding: EdgeInsets.only(left: 40.0),
-            child: Row(
-              children: <Widget>[
-                Text('Kidys Bakery',
-                    style: TextStyle(
-                        fontFamily: 'Montserrat',
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 25.0)),
-                SizedBox(width: 10.0),
-                Text('Categories',
-                    style: TextStyle(
-                        fontFamily: 'Montserrat',
-                        color: Colors.white,
-                        fontSize: 25.0))
-              ],
-            ),
-          ),
-          SizedBox(height: 40.0),
-          Container(
-            height: MediaQuery.of(context).size.height - 185.0,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(topLeft: Radius.circular(75.0)),
-            ),
-            child: ListView(
-              primary: false,
-              padding: EdgeInsets.only(left: 25.0, right: 20.0),
-              children: <Widget>[
-                Padding(
-                    padding: EdgeInsets.only(top: 45.0),
-                    child: Container(
-                        height: MediaQuery.of(context).size.height - 300.0,
-                        child: ListView(
-                            children: [Text("HELLO"), Text("HELLO")]))),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Container(
-                      height: 65.0,
-                      width: 60.0,
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                            color: Colors.grey,
-                            style: BorderStyle.solid,
-                            width: 1.0),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: Center(
-                        child: Icon(Icons.search, color: Colors.black),
-                      ),
-                    ),
-                    Container(
-                      height: 65.0,
-                      width: 60.0,
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                            color: Colors.grey,
-                            style: BorderStyle.solid,
-                            width: 1.0),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: Center(
-                        child: Icon(Icons.shopping_basket, color: Colors.black),
-                      ),
-                    ),
-                    Container(
-                      height: 65.0,
-                      width: 120.0,
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                              color: Colors.grey,
-                              style: BorderStyle.solid,
-                              width: 1.0),
-                          borderRadius: BorderRadius.circular(10.0),
-                          color: Color(0xFF1C1428)),
-                      child: Center(
-                          child: Text('Checkout',
-                              style: TextStyle(
-                                  fontFamily: 'Montserrat',
+                        Container(
+                            width: 125.0,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                IconButton(
+                                  icon: Icon(Icons.work),
                                   color: Colors.white,
-                                  fontSize: 15.0))),
-                    )
-                  ],
-                )
-              ],
+                                  onPressed: () {},
+                                ),
+                                IconButton(
+                                  icon: Icon(Icons.shopping_cart_checkout),
+                                  color: Colors.white,
+                                  onPressed: () {},
+                                ),
+                              ],
+                            ))
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-          )
-        ],
+            SizedBox(height: 25.0),
+            Padding(
+              padding: EdgeInsets.only(left: 40.0),
+              child: Row(
+                children: <Widget>[
+                  Text("Kidy's",
+                      style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25.0)),
+                  SizedBox(width: 10.0),
+                  Text('Categories',
+                      style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          color: Colors.white,
+                          fontSize: 25.0))
+                ],
+              ),
+            ),
+            SizedBox(height: 40.0),
+            Container(
+              height: MediaQuery.of(context).size.height - 18.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(75.0)),
+              ),
+              child: ListView(
+                primary: false,
+                padding: EdgeInsets.only(left: 25.0, right: 20.0),
+                children: <Widget>[
+                  Padding(
+                      padding: EdgeInsets.only(top: 45.0),
+                      child: Container(
+                          height: MediaQuery.of(context).size.height - 300.0,
+                          child: ListView(children: [
+                            Container(
+                              child: Text("HELLO"),
+                              height: 160,
+                            ),
+                            Container(
+                              child: Text("HELLO"),
+                              height: 160,
+                            ),
+                            Container(
+                              child: Text("HELLO"),
+                              height: 160,
+                            ),
+                            Container(
+                              child: Text("HELLO"),
+                              height: 160,
+                            ),
+                            Container(
+                              child: Text("HELLO"),
+                              height: 160,
+                            ),
+                            Container(
+                              child: Text("HELLO"),
+                              height: 160,
+                            ),
+                            Container(
+                              child: Text("HELLO"),
+                              height: 160,
+                            )
+                          ]))),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }

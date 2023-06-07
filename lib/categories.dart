de817f59c66330242e7ebfa5045e703ebc02030e
Sanchit Batra <sanchitbatra@Sanchits-MacBook-Air.local>
2023-06-07 23:25:18 +0530

    Achieving fixed appbars

--- lib/categories.dart
+++ lib/categories.dart
@@ -13,112 +13,117 @@ class _CategoriesState extends State<Categories> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Color(0xFFDD0E1C),
-        body: ListView(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(top: 15.0, left: 10.0),
-              child: Column(
-                children: [
+        body: CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              pinned: true,
+              backgroundColor: Color(0xFFDD0E1C),
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.arrow_back_ios),
+                    color: Colors.white,
+                    onPressed: () {},
+                  ),
                   Container(
+                    width: 125.0,
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: <Widget>[
                         IconButton(
-                          icon: Icon(Icons.arrow_back_ios),
+                          icon: Icon(Icons.work),
                           color: Colors.white,
                           onPressed: () {},
                         ),
-                        Container(
-                            width: 125.0,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                IconButton(
-                                  icon: Icon(Icons.work),
-                                  color: Colors.white,
-                                  onPressed: () {},
-                                ),
-                                IconButton(
-                                  icon: Icon(Icons.shopping_cart_checkout),
-                                  color: Colors.white,
-                                  onPressed: () {},
-                                ),
-                              ],
-                            ))
+                        IconButton(
+                          icon: Icon(Icons.shopping_cart_checkout),
+                          color: Colors.white,
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildListDelegate(
+                [
+                  SizedBox(height: 25.0),
+                  Padding(
+                    padding: EdgeInsets.only(left: 40.0),
+                    child: Row(
+                      children: <Widget>[
+                        Text("Kidy's",
+                            style: TextStyle(
+                                fontFamily: 'Montserrat',
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 25.0)),
+                        SizedBox(width: 10.0),
+                        Text('Categories',
+                            style: TextStyle(
+                                fontFamily: 'Montserrat',
+                                color: Colors.white,
+                                fontSize: 25.0))
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 40.0),
+                  Container(
+                    height: MediaQuery.of(context).size.height - 18.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius:
+                          BorderRadius.only(topLeft: Radius.circular(75.0)),
+                    ),
+                    child: ListView(
+                      primary: false,
+                      padding: EdgeInsets.only(left: 25.0, right: 20.0),
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(top: 45.0),
+                          child: Container(
+                            height: MediaQuery.of(context).size.height - 300.0,
+                            child: ListView(children: [
+                              Container(
+                                child: Text("HELLO"),
+                                height: 160,
+                              ),
+                              Container(
+                                child: Text("HELLO"),
+                                height: 160,
+                              ),
+                              Container(
+                                child: Text("HELLO"),
+                                height: 160,
+                              ),
+                              Container(
+                                child: Text("HELLO"),
+                                height: 160,
+                              ),
+                              Container(
+                                child: Text("HELLO"),
+                                height: 160,
+                              ),
+                              Container(
+                                child: Text("HELLO"),
+                                height: 160,
+                              ),
+                              Container(
+                                child: Text("HELLO"),
+                                height: 160,
+                              ),
+                            ]),
+                          ),
+                        ),
                       ],
                     ),
                   ),
                 ],
               ),
             ),
-            SizedBox(height: 25.0),
-            Padding(
-              padding: EdgeInsets.only(left: 40.0),
-              child: Row(
-                children: <Widget>[
-                  Text("Kidy's",
-                      style: TextStyle(
-                          fontFamily: 'Montserrat',
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 25.0)),
-                  SizedBox(width: 10.0),
-                  Text('Categories',
-                      style: TextStyle(
-                          fontFamily: 'Montserrat',
-                          color: Colors.white,
-                          fontSize: 25.0))
-                ],
-              ),
-            ),
-            SizedBox(height: 40.0),
-            Container(
-              height: MediaQuery.of(context).size.height - 18.0,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(75.0)),
-              ),
-              child: ListView(
-                primary: false,
-                padding: EdgeInsets.only(left: 25.0, right: 20.0),
-                children: <Widget>[
-                  Padding(
-                      padding: EdgeInsets.only(top: 45.0),
-                      child: Container(
-                          height: MediaQuery.of(context).size.height - 300.0,
-                          child: ListView(children: [
-                            Container(
-                              child: Text("HELLO"),
-                              height: 160,
-                            ),
-                            Container(
-                              child: Text("HELLO"),
-                              height: 160,
-                            ),
-                            Container(
-                              child: Text("HELLO"),
-                              height: 160,
-                            ),
-                            Container(
-                              child: Text("HELLO"),
-                              height: 160,
-                            ),
-                            Container(
-                              child: Text("HELLO"),
-                              height: 160,
-                            ),
-                            Container(
-                              child: Text("HELLO"),
-                              height: 160,
-                            ),
-                            Container(
-                              child: Text("HELLO"),
-                              height: 160,
-                            )
-                          ]))),
-                ],
-              ),
-            )
           ],
         ),
       ),

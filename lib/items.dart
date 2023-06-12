2afecbbf5b9ee99a121fb5b817501c12915efeae
Sanchit Batra <sanchitbatra@Sanchits-MacBook-Air.local>
2023-06-12 05:54:02 +0530

    Adding remove focus from search

--- lib/items.dart
+++ lib/items.dart
@@ -46,106 +46,112 @@ class _ItemsState extends State<Items> {
   Widget build(BuildContext context) {
     var items = Provider.of<CategoriesProvider>(context).items;
     return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        body: _isLoading
-            ? const Center(
-                child: SpinKitPulse(
-                color: Color(0xffDD0E1C),
-                size: 50.0,
-              ))
-            : Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    decoration: const BoxDecoration(
-                        gradient: LinearGradient(
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
-                            colors: [Color(0XFF552018), Color(0xff552018)])),
-                    height: 100,
-                    padding: EdgeInsets.all(10),
-                    child: Row(children: [
-                      Flexible(
-                        flex: 1,
-                        child: IconButton(
-                          icon: Icon(Icons.arrow_back_ios_new),
-                          color: Colors.white,
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                      ),
-                      Flexible(
-                        flex: 5,
-                        fit: FlexFit.tight,
-                        child: SizedBox(
-                          height: 45,
-                          child: Card(
-                            clipBehavior: Clip.antiAliasWithSaveLayer,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                            child: CupertinoSearchTextField(
-                              onTap: () {
-                                setState(() {
-                                  _isSearching = true;
-                                });
-                              },
-                              style: const TextStyle(
-                                  fontSize: 18, fontWeight: FontWeight.bold),
-                              controller: searchItemController,
-                              onChanged: (text) {
-                                //onSearch(text);
-                              },
-                            ),
-                          ),
-                        ),
-                      ),
-                      Flexible(
-                        child: Consumer<CartProvider>(
-                          builder: (_, cart, ch) => CartBadge(
-                            value: cart.itemCount.toString(),
-                            color: Colors.red,
-                            child: ch!,
-                          ),
+      child: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          body: _isLoading
+              ? const Center(
+                  child: SpinKitPulse(
+                  color: Color(0xffDD0E1C),
+                  size: 50.0,
+                ))
+              : Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      decoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                              colors: [Color(0XFF552018), Color(0xff552018)])),
+                      height: 100,
+                      padding: EdgeInsets.all(10),
+                      child: Row(children: [
+                        Flexible(
+                          flex: 1,
                           child: IconButton(
+                            icon: Icon(Icons.arrow_back_ios_new),
+                            color: Colors.white,
                             onPressed: () {
-                              //moveToCart(context);
+                              Navigator.of(context).pop();
                             },
-                            icon: const Icon(
-                              Icons.shopping_cart,
-                              color: Colors.white,
-                            ),
-                            iconSize: 30,
                           ),
                         ),
-                      )
-                    ]),
-                  ),
-                  Flexible(
-                    flex: 5,
-                    child: Container(
-                      padding: const EdgeInsets.all(10),
-                      child: GridView.builder(
-                          itemCount: items.length,
-                          primary: false,
-                          gridDelegate:
-                              const SliverGridDelegateWithFixedCrossAxisCount(
-                                  crossAxisCount: 2,
-                                  childAspectRatio: 0.6,
-                                  crossAxisSpacing: 10,
-                                  mainAxisSpacing: 10),
-                          itemBuilder: (context, index) => Item(
-                                imgPath: items[index].imgUrl,
-                                price: items[index].delhi_ncr_price.toString(),
-                                itemId: items[index].id,
-                                itemName: items[index].itemName,
-                              )),
+                        Flexible(
+                          flex: 5,
+                          fit: FlexFit.tight,
+                          child: SizedBox(
+                            height: 45,
+                            child: Card(
+                              clipBehavior: Clip.antiAliasWithSaveLayer,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              child: CupertinoSearchTextField(
+                                onTap: () {
+                                  setState(() {
+                                    _isSearching = true;
+                                  });
+                                },
+                                style: const TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                                controller: searchItemController,
+                                onChanged: (text) {
+                                  //onSearch(text);
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
+                        Flexible(
+                          child: Consumer<CartProvider>(
+                            builder: (_, cart, ch) => CartBadge(
+                              value: cart.itemCount.toString(),
+                              color: Colors.red,
+                              child: ch!,
+                            ),
+                            child: IconButton(
+                              onPressed: () {
+                                //moveToCart(context);
+                              },
+                              icon: const Icon(
+                                Icons.shopping_cart,
+                                color: Colors.white,
+                              ),
+                              iconSize: 30,
+                            ),
+                          ),
+                        )
+                      ]),
                     ),
-                  )
-                ],
-              ),
+                    Flexible(
+                      flex: 5,
+                      child: Container(
+                        padding: const EdgeInsets.all(10),
+                        child: GridView.builder(
+                            itemCount: items.length,
+                            primary: false,
+                            gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                                    crossAxisCount: 2,
+                                    childAspectRatio: 0.6,
+                                    crossAxisSpacing: 10,
+                                    mainAxisSpacing: 10),
+                            itemBuilder: (context, index) => Item(
+                                  imgPath: items[index].imgUrl,
+                                  price:
+                                      items[index].delhi_ncr_price.toString(),
+                                  itemId: items[index].id,
+                                  itemName: items[index].itemName,
+                                )),
+                      ),
+                    )
+                  ],
+                ),
+        ),
       ),
     );
   }

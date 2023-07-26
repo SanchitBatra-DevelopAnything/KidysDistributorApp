e05ef9b9810d1243939cfd4604af71739612df93
Sanchit Batra <sanchitbatra@Sanchits-MacBook-Air.local>
2023-07-26 07:49:16 +0530

    Fixing button width

--- lib/itemCounterButton.dart
+++ lib/itemCounterButton.dart
@@ -48,91 +48,94 @@ class _CountButtonViewState extends State<CountButtonView> {
       quantity = count;
     });
 
-    return SizedBox(
-      width: double.infinity - 100,
-      height: 50.0,
-      child: Center(
-        child: Container(
-          decoration: BoxDecoration(
-            color: Color(0xffffffff),
-            border: Border.all(color: Colors.white, width: 1.0),
-            borderRadius: BorderRadius.circular(22.0),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              GestureDetector(
-                onTap: () {
-                  updateCount(-1);
-                },
-                onLongPress: () {
-                  updateCount(-50);
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(22.0),
-                  ),
-                  width: quantity < 100 ? 40 : 32,
-                  child: Center(
-                    child: Text(
-                      '-',
-                      style: TextStyle(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold,
-                        color: Color.fromARGB(255, 4, 102, 7),
-                        decoration: TextDecoration.none,
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity - 100,
+        height: 50.0,
+        child: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Color(0xffffffff),
+              border: Border.all(color: Colors.white, width: 1.0),
+              borderRadius: BorderRadius.circular(22.0),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                GestureDetector(
+                  onTap: () {
+                    updateCount(-1);
+                  },
+                  onLongPress: () {
+                    updateCount(-50);
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(22.0),
+                    ),
+                    width: quantity < 100 ? 35 : 20,
+                    child: Center(
+                      child: Text(
+                        '-',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 4, 102, 7),
+                          decoration: TextDecoration.none,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              AnimatedSwitcher(
-                duration: const Duration(milliseconds: 200),
-                transitionBuilder: (Widget child, Animation<double> animation) {
-                  return ScaleTransition(
-                    scale: animation,
-                    child: child,
-                  );
-                },
-                child: Text(
-                  '$quantity',
-                  key: ValueKey<int>(quantity),
-                  style: TextStyle(
-                    fontSize: quantity < 100 ? 18.0 : 15.0,
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(255, 4, 102, 7),
-                    decoration: TextDecoration.none,
+                AnimatedSwitcher(
+                  duration: const Duration(milliseconds: 200),
+                  transitionBuilder:
+                      (Widget child, Animation<double> animation) {
+                    return ScaleTransition(
+                      scale: animation,
+                      child: child,
+                    );
+                  },
+                  child: Text(
+                    '$quantity',
+                    key: ValueKey<int>(quantity),
+                    style: TextStyle(
+                      fontSize: quantity < 100 ? 15.0 : 15.0,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 4, 102, 7),
+                      decoration: TextDecoration.none,
+                    ),
                   ),
                 ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  updateCount(1);
-                },
-                onLongPress: () {
-                  updateCount(50);
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(22.0),
-                    color: Colors.white,
-                  ),
-                  width: quantity < 100 ? 40 : 32,
-                  child: Center(
-                    child: Text(
-                      '+',
-                      style: TextStyle(
-                        fontSize: quantity < 100 ? 18.0 : 15.0,
-                        fontWeight: FontWeight.bold,
-                        color: Color.fromARGB(255, 4, 102, 7),
-                        decoration: TextDecoration.none,
+                GestureDetector(
+                  onTap: () {
+                    updateCount(1);
+                  },
+                  onLongPress: () {
+                    updateCount(50);
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(22.0),
+                      color: Colors.white,
+                    ),
+                    width: quantity < 100 ? 32 : 32,
+                    child: Center(
+                      child: Text(
+                        '+',
+                        style: TextStyle(
+                          fontSize: quantity < 100 ? 15.0 : 15.0,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 4, 102, 7),
+                          decoration: TextDecoration.none,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
